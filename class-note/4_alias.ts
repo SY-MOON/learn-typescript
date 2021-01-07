eaa4cd85010d2642b723cc047c3b831927d73d97
Emma Moon <emma@freshcode.me>
2021-01-07 23:17:23 +0900

    타입 별칭

---
+++ class-note/4_alias.ts
@@ -0,0 +1,22 @@
+type Person2 = {
+  name: string;
+  age: number;
+}
+
+// interface Person2  {
+//   name: string;
+//   age: number;
+// }
+
+var seho: Person2 = {
+  name: '세호',
+  age: 30
+}
+
+type MyString = string;
+var strrr: MyString = 'hello';
+
+type Todo = { id: string; title: string; done: boolean; }
+function getTodo(todo: Todo) {
+  
+}
