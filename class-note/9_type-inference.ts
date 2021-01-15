97115e04bc9dcfed80be4d87b90b746b4eb2dfc5
Emma Moon <emma@freshcode.me>
2021-01-15 21:02:06 +0900

    타입추론

---
+++ class-note/9_type-inference.ts
@@ -0,0 +1,36 @@
+var a = 10;
+
+function getB(b = 10) {
+  var c = 'hi';
+  return b + c;
+}
+
+//2
+// interface Dropdown<T> {
+//   value: T;
+//   title: string;
+// }
+
+// var shoppingItem: Dropdown<string> = {
+//   value: '',
+//   title: ''
+// }
+
+
+//3
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+
+interface DetailedDropdown<K> extends Dropdown<K> {
+  description: string;
+  tag: K;
+}
+  
+var detailedItem: DetailedDropdown<string> = {
+  title: 'abc', 
+  description: 'ab',
+  value: '',
+  tag: ''
+}
